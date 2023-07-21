768968080fd79b2dcceabdde6697b83b0e3ed4e7
Halibei Marian <marianhalibei123@gmail.com>
2023-07-21 15:50:25 +0200

    Solved the Query the DB exercise

--- queries.py
+++ queries.py
@@ -1,16 +1,34 @@
-# pylint:disable=C0111,C0103
+import sqlite3
+conn = sqlite3.connect('data/ecommerce.sqlite')
+db = conn.cursor()
 
 def query_orders(db):
     # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    results = db.execute("SELECT * FROM Orders ORDER BY OrderID ASC")
+    results = results.fetchall()
+    return results
+
+
+
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
+    query = """SELECT *
+      FROM Orders
+      WHERE OrderDate >  ?  AND OrderDate   < ? """
 
+    db.execute(query, (date_from, date_to))
+    results = db.fetchall()
+    return results
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    results = db.execute ("""SELECT *,
+                         JULIANDAY(ShippedDate) - JULIANDAY(OrderDate) AS TimeDelta
+                        FROM Orders
+                        ORDER BY TimeDelta""")
+
+    results = results.fetchall()
+    return results
